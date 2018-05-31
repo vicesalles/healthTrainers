0522b746338db0b88262a159683fdd9abb3366aa
Vice Sallés <vicenc.salles@gmail.com>
2018-05-31 07:21:25 +0200

    feat: CTA functionallity

---
+++ js/index.js
@@ -0,0 +1,14 @@
+//Call To Action Button
+
+document.addEventListener('DOMContentLoaded',()=>{
+
+    const cta = document.querySelector('.CTA');
+    cta.addEventListener('click',()=>{
+
+        alert('Gràcies, però aquest curs és un exercici. +info al peu de pàgina')
+
+    })
+
+
+});
+
